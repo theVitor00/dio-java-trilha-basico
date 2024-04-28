d1f69564d113ea7d64938f6b3be201f91ce9e310
Pablo Vitor <thevitor00@gmail.com>
2024-04-27 22:27:02 -0300

    update: mudancça de IDE e primeiros arquivos

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,19 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salarioMinimo = 1275.33;
+
+        System.out.printf("IDADE: %d%n", idade);
+        System.out.printf("ANO:%d%n", ano);
+        System.out.printf("CEP: %d%n", cep);
+        System.out.printf("CPF: %d%n", cpf);
+        System.out.printf("PI: %f%n", pi);
+        System.out.printf("SALARIO MINIMO: %f%n", salarioMinimo);
+    }
+}
