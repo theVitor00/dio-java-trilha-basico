1ef65541eb135451bbf430f294aca2a472c477c8
Pablo Vitor <thevitor00@gmail.com>
2024-04-28 21:37:06 -0300

    fix: README

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Operadores {
+import java.util.Date;
+
+public class Operadores {
+
+    public static void main (String[] args) {
+
+        // Atribuição
+        String nome = "Pablo";
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+        // Aritméticos
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 18 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 + 7) + (20 / 4.0);
+    }
 }
